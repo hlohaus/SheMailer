ebe5219e0bc6fc3a8435fb1ef39bdb6f00c77f0a
Heiner Lohaus <heiner.lohaus@avency.de>
2020-01-13 15:17:18 +0100

    Add smtp support

--- src/SheMailer.php
+++ src/SheMailer.php
@@ -2,9 +2,29 @@
 
 namespace She\Mailer;
 
+use She\Mailer\Components\ConfigListener;
+use She\Mailer\Components\RedirectingListener;
 use Shopware\Core\Framework\Plugin;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
 
-class SheMailer extends Plugin
+class SheMailer extends Plugin implements CompilerPassInterface
 {
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+        $container->addCompilerPass($this);
+    }
 
+    public function process(ContainerBuilder $container)
+    {
+        if ($container->has('core_mailer')) {
+            $container->getDefinition('core_mailer')->addMethodCall('registerPlugin', [
+                new Reference(ConfigListener::class)
+            ])->addMethodCall('registerPlugin', [
+                new Reference(RedirectingListener::class)
+            ]);
+        }
+    }
 }
